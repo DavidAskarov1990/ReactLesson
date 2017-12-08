fb15311c23ac0fd06e25d36e24d3adcbd80a647d
DavidAskarov1990 <happypeople.sleep@gmail.com>
2017-12-08 20:03:45 +0200

    config

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -11,9 +11,20 @@ export default function (app, passport) {
     app.set("host", "localhost");
 
 
-    // Заголовок X-Powered-By не имеет функционального значения.
-    // Сохранение этого облегчает для злоумышленника создание профиля сайта
-    // Это можно безопасно удалить
-    app.disable("x-powered-by")
+    /** Заголовок X-Powered-By не имеет функционального значения.
+        Сохранение этого облегчает для хакера создание профиля сайта
+        Это можно безопасно удалить
+    */
+    app.disable("x-powered-by");
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    /**
+     * process.cwd() - возвращает текущий рабочий каталог
+     * */
+    app.use(express.static(path.join(process.cwd(), 'public')));
+
+    console.log(`Current directory: ${process.cwd()}`);
 
 }
